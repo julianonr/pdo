6d9e92aeb9e1d4607c1bdc3db46014d142ccf0dc
juliano Nizer Ribas <julianonr@gmail.com>
2021-09-08 14:40:23 -0300

    Limpando o repositprio

--- index.php
+++ index.php
@@ -4,26 +4,8 @@ namespace App;
 
 class Main
 {
-
-    private $name = 'Juliano';
-
     public function __construct()
     {
         echo "oi";
     }
-
-    /**
-     * Undocumented function
-     *
-     * @param string $name
-     * @return void
-     */
-    public function teste(string $name)
-    {
-        return $name;
-    }
 }
-
-$x = new Main();
-
-var_dump $x->__construct();
